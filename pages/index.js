6fafb74e9dde6509e0462925370cacfb14bb5878
Nicolas Blaisdell <nblaisdell2@gmail.com>
2021-12-18 15:03:34 -0500

    adding minimal styling to the app for logout button

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to the APP</h1>
-      <button onClick={logout}>Logout</button>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-2xl my-10">Welcome to the METAVERSE!</div>
+        <button
+          className="bg-yellow-500 p-3 rounded-lg font-bold hover:animate-pulse hover:underline"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
